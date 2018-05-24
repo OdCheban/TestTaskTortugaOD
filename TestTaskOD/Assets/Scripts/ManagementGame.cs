6b99f5da8f9b03b1d29dc9da8a3de36cc20dcbf0
OdCheban <odcheban@gmail.com>
2018-05-24 10:48:48 +0300

    add UITextPoints

--- TestTaskOD/Assets/Scripts/ManagementGame.cs
+++ TestTaskOD/Assets/Scripts/ManagementGame.cs
@@ -1,19 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ManagementGame : MonoBehaviour {
     PlatformBlock[] platform;
     int kBlock;
 
+    Text textPoints;
     private int _gamePoints;
     public int GamePoints
     {
         get { return _gamePoints; }
-        set { _gamePoints = value; }
+        set {
+            _gamePoints = value;
+            textPoints.text = _gamePoints.ToString();
+        }
     }
     private void Start()
     {
+        textPoints = GameObject.Find("Canvas/PointsText").GetComponent<Text>();
         platform = transform.Find("Platform").GetComponentsInChildren<PlatformBlock>();
         kBlock = platform.Length;
     }
